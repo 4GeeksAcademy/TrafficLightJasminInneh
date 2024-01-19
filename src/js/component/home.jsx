d1412032e462febc1b6b7086757aaf012b2db8d9
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-01-19 14:44:30 +0000

    added the glow to the traffic lights

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,12 +5,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	const [ selectedColor, setSelectedColor ] = useState("red")
+	const [ selectedColor, setSelectedColor ] = useState("green");
 	return (
 		<div className="traffic-light">
-			<div className={"light red"+(selectedColor === "red")? " glow" : ""}></div>
-			<div className={"light yellow"+(selectedColor === "yellow")? " glow" : ""}></div>
-			<div className={"light green"+(selectedColor === "green")? " glow" : ""}></div>
+			<div onClick={() => setSelectedColor("red")} className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+			<div onClick={() => setSelectedColor("yellow")} className={"light yellow"+(selectedColor === "yellow"? " glow" : "")}></div>
+			<div onClick={() => setSelectedColor("green")} className={"light green"+(selectedColor === "green"? " glow" : "")}></div>
 		</div>
 	)
 }
